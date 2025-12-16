965a5a19980dac0671ace4d27139ea96d20fb066
Dan Knorr <Daniel.Knorr@gmail.com>
2025-12-16 15:31:16 -0500

    Updated import.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.os_manager import ChromeType
+from webdriver_manager.core.utils import ChromeType
 from time import sleep
 from datetime import datetime, timedelta, time
 import pandas as pd
